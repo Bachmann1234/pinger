c054da1869184854789c694a73ecd9a10e59e01c
Matt Bachmann <bachmann.matt@gmail.com>
2020-07-05 17:15:51 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Pinger
+
+```
+venv ❯ python pinger.py --help
+usage: pinger.py [-h] [--initial_delay INITIAL_DELAY]
+                 [--time_between_pongs TIME_BETWEEN_PONGS]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --initial_delay INITIAL_DELAY, -i INITIAL_DELAY
+  --time_between_pongs TIME_BETWEEN_PONGS, -t TIME_BETWEEN_PONGS
+```
+
+Sound is from: https://freesound.org/people/Natty23/sounds/411749/
+
---
+++ pinger.py
@@ -0,0 +1,41 @@
+from playsound import playsound
+from time import sleep
+import sys
+import argparse
+
+ERASE_IN_LINE_TERM_ESCAPE = "\033[K"
+TONE_SOUND = "tone.wav"
+
+def print_replaceable_message(msg):
+    sys.stdout.write(ERASE_IN_LINE_TERM_ESCAPE)
+    print("\t" + msg, end="\r")
+
+
+def countdown(seconds):
+    for tick in range(seconds):
+        print_replaceable_message(str(seconds - tick))
+        sleep(1)
+
+
+def main(initial_delay, time_between_pongs):
+    try:
+        countdown(initial_delay)
+        while True:
+            countdown(time_between_pongs)
+            print_replaceable_message("DING!")
+            playsound(TONE_SOUND)
+    except KeyboardInterrupt:
+        sys.stdout.write(ERASE_IN_LINE_TERM_ESCAPE)
+        print("\b\bGoodbye!")
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--initial_delay", "-i", type=int, default=0)
+    parser.add_argument("--time_between_pongs", "-t", type=int, default=4)
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = get_args()
+    main(args.initial_delay, args.time_between_pongs)
